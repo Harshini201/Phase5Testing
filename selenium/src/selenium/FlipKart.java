6ce1b1a467c256651ce0336c45eb53b085ac9cbf
Hasrh <arasuharshini201@gmail.com>
2022-10-04 14:51:31 +0530

    commit

---
+++ selenium/src/selenium/FlipKart.java
@@ -0,0 +1,24 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FlipKart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Harshini Arasu\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		System.out.println("The search for :"+driver.getTitle());
+		WebElement searchIcon = driver.findElement(By.name("q"));//id locator for next button
+        
+        searchIcon.click();
+    System.out.println(searchIcon.getAttribute("title"));
+		
+
+	}
+
+}
